5ee47a2153789d4b2e707e71569c98498297fee0
David Fisher <fisherds@rose-hulman.edu>
2020-04-28 14:06:28 -0400

    finished DIY UITableView via a UIViewController

---
+++ MovieQuotes/TempViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TempViewController.swift
+//  MovieQuotes
+//
+//  Created by David Fisher on 4/28/20.
+//  Copyright © 2020 David Fisher. All rights reserved.
+//
+
+import UIKit
+
+class TempViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  let tempCellIdentifier = "TempCell"
+
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 5
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: tempCellIdentifier, for: indexPath)
+    // Configure the cell
+    cell.textLabel?.text = "This is row \(indexPath.row)"
+    return cell
+  }
+}
