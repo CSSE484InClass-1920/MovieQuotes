6ededaa9babf3c73bb290904980d54a692422e74
David Fisher <fisherds@rose-hulman.edu>
2020-05-01 12:18:57 -0400

    delete with Firebase

--- MovieQuotes/MovieQuotesTableViewController.swift
+++ MovieQuotes/MovieQuotesTableViewController.swift
@@ -97,8 +97,10 @@ class MovieQuotesTableViewController: UITableViewController {
   
   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
     if editingStyle == .delete {
-      movieQuotes.remove(at: indexPath.row)
-      tableView.reloadData()
+//      movieQuotes.remove(at: indexPath.row)
+//      tableView.reloadData()
+      let movieQuoteToDelete = movieQuotes[indexPath.row]
+      movieQuotesRef.document(movieQuoteToDelete.id!).delete()
     }
   }
   
