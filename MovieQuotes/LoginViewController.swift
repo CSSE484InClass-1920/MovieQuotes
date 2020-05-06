e4a447d6d4e43195b47df0df7fadabd5d1a71bd9
David Fisher <fisherds@rose-hulman.edu>
2020-05-06 14:37:52 -0400

    sign in and log in via Email password works!

--- MovieQuotes/LoginViewController.swift
+++ MovieQuotes/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -15,9 +16,37 @@ class LoginViewController: UIViewController {
 
   let showListSegueIndentifier = "ShowListSegue"
 
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    emailTextField.placeholder = "Email"
+    passwordTextField.placeholder = "Password"
+  }
+
   @IBAction func pressedSignInNewUser(_ sender: Any) {
+    let email = emailTextField.text!
+    let password = passwordTextField.text!
+    Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+      if let error = error {
+        print("Error creating a new user for Email/Password \(error)")
+        return
+      }
+      print("It worked!  A new user is created and now signed in!")
+      print("Email \(authResult!.user.email!)  UID: \(authResult!.user.uid)")
+      self.performSegue(withIdentifier: self.showListSegueIndentifier, sender: self)
+    }
   }
 
   @IBAction func pressedLogInExistingUser(_ sender: Any) {
+    let email = emailTextField.text!
+    let password = passwordTextField.text!
+    Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+      if let error = error {
+        print("Error logging in an existing user for Email/Password \(error)")
+        return
+      }
+      print("It worked!  Signed in an existing user!")
+      print("Email \(authResult!.user.email!)  UID: \(authResult!.user.uid)")
+      self.performSegue(withIdentifier: self.showListSegueIndentifier, sender: self)
+    }
   }
 }
